7d38986d9a03ea459e90a2e32833a8267ea22bd0
GabrielF <ogabrielfef@gmail.com>
2022-07-14 17:59:12 -0300

    Req 4 e 6 tests

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Login from '../pages/Login';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
 describe('Testa a tela de Login', () => {
   test('testa os inputs e o botão', () => {
@@ -15,4 +18,19 @@ describe('Testa a tela de Login', () => {
     expect(inputEmail).toBeInTheDocument();
     expect(buttonEnter).toBeInTheDocument();
     });
+
+  test('testa se o button esta habilitado', () => {
+    // console.log(renderWithRouter);
+    renderWithRouter(<App />)
+    // acessar os elementos da tela
+    const email = 'ogabriel@gmail.com';
+    // interagir com os elementos (se for necessário)
+    userEvent.type(screen.getByTestId("email-input"), email);
+    userEvent.type(screen.getByTestId("password-input"), '1234567');
+    userEvent.click(screen.getByTestId("login-submit-btn"));
+    const localStorageUser = JSON.parse(localStorage.getItem('user'));
+    // console.log(localStorageUser);
+    // fazer os testes
+    expect(localStorageUser.email).toBe(email);
   });
+});
