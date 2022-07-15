cdcc685f781f31675f09bac86ecf6f64a78c1b7d
kaduh15 <kadu.silva2014@gmail.com>
2022-07-15 18:19:09 -0300

    resolve o requesito 12

--- src/context/FoodProvider.jsx
+++ src/context/FoodProvider.jsx
@@ -1,30 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import foodContext from './FoodContext';
-import { getFoodBy } from '../services/foodAPI';
 
 function Provider({ children }) {
   const [searchBar, setSearchBar] = useState({ input: '', radio: '' });
-  const [resultSearchBar, setResultSearchBar] = useState([]);
 
   const handleSearchBar = ({ inputValue, radioValue }) => {
     setSearchBar({ input: inputValue, radio: radioValue });
   };
 
   const state = {
-    handleSearchBar, resultSearchBar,
+    handleSearchBar, searchBar,
   };
-
-  useEffect(() => {
-    if (searchBar.input.length > 1 && searchBar.radio === 'f') {
-      global.alert('Your search must have only 1 (one) character');
-    } else {
-      (async () => {
-        setResultSearchBar(await getFoodBy(searchBar.input, searchBar.radio));
-      })();
-    }
-  }, [searchBar]);
 
   return (
     <foodContext.Provider value={ state }>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,54 @@
 import React from 'react';
+import Svg from '../components/Svg';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+
+import './details.css';
 
 export default function Details() {
-  return <h1>Details</h1>;
+  return (
+    <>
+      <div className="card">
+        <img
+          className="image-card"
+          src="https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg"
+          alt="Big Mac"
+        />
+        <div className="card-inner">
+          <div className="title-subtitle-card">
+            <h1 className="title-card">Big Mac </h1>
+            <p className="category-card">Beef</p>
+          </div>
+          <div className="icons-card">
+            <Svg src={ shareIcon } />
+            <Svg src={ whiteHeartIcon } />
+          </div>
+        </div>
+
+      </div>
+      <div className="content-wrapper">
+        <h2>Ingredients</h2>
+        <div>
+          <ul>
+            <li>Minced Beef - 400g</li>
+            <li>Olive Oil - 2 tbs</li>
+            <li>Sesame Seed Burger Buns - 2</li>
+            <li>Onion - Chopped</li>
+            <li>Iceberg Lettuce - 1/4</li>
+            <li>...</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Instructions</h2>
+          <p>
+            For the Big Mac sauce, combine all the ingredients in a bowl, season...
+          </p>
+        </div>
+        <div>
+          <h2>Video</h2>
+          <div>player de Video</div>
+        </div>
+      </div>
+    </>
+  );
 }
