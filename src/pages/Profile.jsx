cf3cf27d9f98e8a62e708546a1da695eed5d871c
Camilla Geber <camilla.geber@gmail.com>
2022-07-15 17:45:40 -0300

    Reqs 57, 59 e 61. Início do 60

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default function Profile() {
+export default function Profile({ history }) {
   return (
     <>
       <Header pageTitle="Profile" />
       <h1>Profile</h1>
+      <h3 data-testid="profile-email">Email</h3>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
     </>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.node.isRequired,
+};
