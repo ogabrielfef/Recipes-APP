0f16a8b76889cdda3875b4ae8b79f7bf23656e29
RafaelSotero-dev <rafaeljesusabc2@gmail.com>
2022-07-20 16:34:35 -0300

    .

--- src/tests/Search.test.js
+++ src/tests/Search.test.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import userEvent from '@testing-library/user-event';
-import SearchBar from '../components/SearchBar';
-import FoodProvider from '../context/FoodProvider';
 import App from '../App';
 
 describe('Testando o SearchBar', () => {
   test('verifica os radio', () => {
     renderWithRouter(<App />, '/foods');
     const buttonSearch = screen.getByTestId('search-top-btn')
-    userEvent.click(buttonSearch)
+    userEvent.click(buttonSearch);
 
     const inputIngredients = screen.getByTestId('ingredient-search-radio');
     expect(inputIngredients).toBeInTheDocument();
@@ -30,12 +28,15 @@ describe('Testando o SearchBar', () => {
   test('testa o inputs', () => {
     renderWithRouter(<App />, '/foods');
     const buttonSearch = screen.getByTestId('search-top-btn')
-    userEvent.click(buttonSearch)
+    userEvent.click(buttonSearch);
 
     const inputState = 'lalaland'
     const input = screen.getByTestId('search-input');
     expect(input).toBeInTheDocument();
     userEvent.type(screen.getByTestId('search-input'), 'lalaland');
     expect(input.value).toBe(inputState);
+
+    const buttonSearch2 = screen.getByTestId('exec-search-btn');
+    userEvent.click(buttonSearch2);
   });
 });
