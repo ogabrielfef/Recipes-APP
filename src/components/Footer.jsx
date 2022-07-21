88537984b6884989174d7cfb14f562d3b887f777
kaduh15 <kadu.silva2014@gmail.com>
2022-07-21 18:00:31 -0300

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Svg from './Svg';
 
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
-import foodContext from '../context/FoodContext';
 import './Footer.css';
 
 export default function Footer() {
   // const [onRedirectMeal, setOnRedirectMeal] = useState(false);
   // const [onRedirectDrink, setOnRedirectDrinkl] = useState(false);
-  const { setTypeResult } = useContext(foodContext);
   const history = useHistory();
   return (
     <footer data-testid="footer" className="fixed">
@@ -19,7 +17,6 @@ export default function Footer() {
         className="food"
         onClick={ () => {
           history.push('/foods');
-          setTypeResult('foods');
         } }
       >
         <Svg src={ mealIcon } testId="food-bottom-btn" />
@@ -28,7 +25,6 @@ export default function Footer() {
         type="button"
         className="drink"
         onClick={ () => {
-          setTypeResult('drinks');
           history.push('/drinks');
         } }
       >
